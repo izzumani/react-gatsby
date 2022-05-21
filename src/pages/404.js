3e6bc5e901274a735c2a950cc1f5f728c1c09edb
izzumani <isaac.njiru@gmail.com>
2022-05-21 06:01:53 +0300

    update pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,12 @@
-import react from "react"
-
+import React from "react"
+import {Link} from "gatsby"
 export default function NotFound()
 {
     return (
         <div>
             <h1>Oh No!</h1>
             <p>The page you were looking for does not exist.</p>
+            <Link to="/">Take me home</Link>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
-import react from "react"
-
-export default function index (){
+import React from "react"
+import {Link} from "gatsby"
+const Index = () => {
     return (
         <div>
             <h1>My Landing Page</h1>
             <p>This is my landing page.</p>
+            <Link to="/about">About Me</Link>
         </div>
     )
 }
+
+export default Index

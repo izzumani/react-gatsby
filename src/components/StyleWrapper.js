7bf1cdefe06ddafa95d665ab86c376f4c8db1282
izzumani <isaac.njiru@gmail.com>
2022-05-21 09:52:14 +0300

    Add Style Wrapper on each component

---
+++ src/components/StyleWrapper.js
@@ -0,0 +1,8 @@
+import React, {Fragment} from "react"
+import "./StyleWrapper.css"
+
+const StyleWrapper = ({children})=>{
+ <Fragment>{children}</Fragment>
+}
+
+export default StyleWrapper
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 import {Link} from "gatsby"
+import StyleWrapper from "../components/StyleWrapper"
 const Index = () => {
     return (
         <div>
-            <h1>My Landing Page</h1>
-            <p>This is my landing page.</p>
-            <Link to="/about">About Me</Link>
+            <StyleWrapper>
+                <h1>My Landing Page</h1>
+                <p>This is my landing page.</p>
+                <Link to="/about">About Me</Link>
+            </StyleWrapper>
+            
         </div>
     )
 }
